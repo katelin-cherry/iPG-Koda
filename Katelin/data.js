a489d02a2441c00d2a17f12470565dd727caeef5
Katelin Cherry <kdcherry55@gmail.com>
2020-04-28 18:29:58 -0500

    Katelin specific questions

---
+++ Katelin/data.js
@@ -0,0 +1,36 @@
+// Questions
+var q = [
+	'Whats your unique insight?',
+	'What’s stopping other companies from replicating this?',
+	'Cant one of the EMR companies easily replicate what youre doing and do it better?',
+	'Who is going to do the machine learning coding for your IP? ',
+	'Why isnt someone already doing this?',
+	'What have you learned so far from working on your product? ',
+	'What are the top things your users want?',
+	'What has surprised you about user behavior? ',
+	'Whats new about what you make?',
+	'Where is the rocket science here?',
+	'How does your product work in more detail?',
+	'What do you understand about your users?',
+	'What domain expertise do you have?',
+	'What else have you created together? ',
+	'What part of your project are you going to build first?',
+	'What are you going to do next?',
+	'What is the next step with the product evolution?',
+	'What systems have you hacked?',
+	'Whats the biggest mistake you have made?',
+	'Whos the boss?',
+	'Why are you uniquely qualified to work on this?',
+	'What, exactly, makes you different from existing options?'
+];
+
+// Tips
+var tips = [
+	'Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+	'When talking to PG and the other YC partners, be substantive, and get straight to the point.',
+	'DO NOT BEAT AROUND THE BUSH. Do not hem and haw. "Uhm"s and "uh"s don&#39;t lend an aura of intelligence.',
+	'Use precise and concise language. You aren&#39;t pitching a VC that will ask you what your "secret sauce" is.',
+	'He talks quickly, and so do the other YC partners. Try to match their tempo (as you should when talking to anyone).'
+];
